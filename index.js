8705df4e20d2fe3072b0794860432912ba82b858
sambasivabat12-debug <sambasiva.bat12@gmail.com>
2026-06-10 23:42:56 +0530

    Add routing for blog post URLs to template
    Route blog post URLs to a specific template for rendering.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    let path = url.pathname;
-    if (!path.match(/\.[a-zA-Z0-9]+$/) && !path.endsWith('/')) {
-      const newRequest = new Request(
-        new URL(path + '.html', url), request
-      );
-      return env.ASSETS.fetch(newRequest);
+    const path = url.pathname;
+
+    // Route blog post URLs to post-template.html
+    if (path.startsWith('/blog/') && 
+        !path.match(/\.[a-zA-Z0-9]+$/) && 
+        path !== '/blog/' && 
+        path !== '/blog') {
+      const templateUrl = new URL('/blog/post-template.html', url);
+      return env.ASSETS.fetch(new Request(templateUrl.toString(), request));
     }
+
     return env.ASSETS.fetch(request);
   }
 }
